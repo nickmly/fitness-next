fa8802e918d95b9d34c0f0be6834679c8f0c5655
nickmly <nickmly@gmail.com>
2024-07-21 17:10:15 -0400

    loading page for exercises

---
+++ src/app/exercises/[slug]/loading.tsx
@@ -0,0 +1,17 @@
+import { Skeleton } from '@/components/ui/skeleton'
+import { LoaderCircle } from 'lucide-react'
+import React from 'react'
+
+
+const ExerciseLoadingPage = () => {
+    return (
+        <>
+            <div className='w-full flex justify-center items-center'>
+                <LoaderCircle className='animate-spin w-20 h-20' />
+            </div>
+
+        </>
+    )
+}
+
+export default ExerciseLoadingPage
