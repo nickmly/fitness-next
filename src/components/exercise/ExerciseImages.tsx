7f7ba67933ff88945038c1c113db0033280429e0
nickmly <nickmly@gmail.com>
2024-07-21 16:51:11 -0400

    exercise image styling

--- src/components/exercise/ExerciseImages.tsx
+++ src/components/exercise/ExerciseImages.tsx
@@ -11,8 +11,8 @@ const ExerciseImages = ({ exercise }: Props) => {
     return (
         <>
             {exercise &&
-                <div className="flex justify-around md:justify-between gap-3 flex-wrap max-w-3xl">
-                    {exercise.images.map(image => <CldImage key={image} src={image} alt={exercise.name} width={320} height={240} />)}
+                <div className="flex justify-around lg:justify-start gap-3 flex-wrap max-w-3xl lg:max-w-none">
+                    {exercise.images.map(image => <CldImage key={image} src={image} alt={exercise.name} width={540} height={480} className='max-w-full w-80 lg:max-w-lg' />)}
                 </div>
             }
         </>
