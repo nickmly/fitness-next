c65879fc399aa605161ae9847a34e39483ca441e
nickmly <nickmly@gmail.com>
2024-07-27 17:57:14 -0400

    loading state for action buttons

--- src/components/log/LoggedExerciseActionButton.tsx
+++ src/components/log/LoggedExerciseActionButton.tsx
@@ -1,21 +1,28 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '../ui/button'
+import { LoaderCircle } from 'lucide-react'
 
 interface Props {
     className?: string
-    performAction: () => void
+    performAction: () => Promise<void>
     children?: React.ReactNode
 }
 
 const LoggedExerciseActionButton = ({ className, performAction, children }: Props) => {
+    const [actionLoading, setActionLoading] = useState(false)
     return (
         <Button
             variant='edge'
+            disabled={actionLoading}
             className={className}
-            onClick={() => performAction()}>
-            {children}
-        </Button>
+            onClick={async () => {
+                setActionLoading(true)
+                await performAction()
+                setActionLoading(false)
+            }}>
+            {!actionLoading ? children : <LoaderCircle className='animate-spin w-4 h-4' />}
+        </Button >
     )
 }
 
