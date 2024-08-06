cc8477e4d0a6340122d6263214f33a213ff53898
nickmly <nickmly@gmail.com>
2024-08-05 22:05:34 -0400

    index page style fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default async function Home() {
     <>
       <div className='relative w-full h-full min-h-[720px]' style={backgroundStyles}>
         <div className='bg-black/80 absolute top-0 left-0 right-0 h-full' />
-        <div className='absolute z-1 top-1/2 -translate-y-1/2 max-w-5xl flex flex-col p-10' style={{ textShadow: 'black 1px 1px' }}>
-          <h2 className='text-4xl mb-5'>Working out redefined</h2>
+        <div className='absolute z-1 top-1/2 -translate-y-1/2 max-w-5xl flex flex-col p-10'>
+          <h2 className='text-4xl mb-5' style={{ textShadow: 'black 1px 1px' }}>Working out redefined</h2>
           <div className='flex gap-4'>
             <Button asChild variant='default'>
               <Link href='/login' className='uppercase'>
