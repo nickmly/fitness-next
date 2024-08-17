a0f2b8f95833fdda14c49e7d1cb7c48cf548bf48
nickmly <nickmly@gmail.com>
2024-08-17 09:09:43 -0400

    fix overflow on muscle page

--- src/app/exercises/muscle/[muscle]/page.tsx
+++ src/app/exercises/muscle/[muscle]/page.tsx
@@ -19,7 +19,7 @@ const ExerciseMusclePage = async ({ params }: Props) => {
             <h2 className='text-xl font-bold capitalize mb-5'>{params.muscle} exercises</h2>
             <div className='flex flex-col gap-2'>
                 {exercises.map(e =>
-                    <Button asChild key={e.slug} variant='secondary'>
+                    <Button asChild key={e.slug} variant='secondary' className='whitespace-normal'>
                         <Link className='w-full' href={`/exercises/${e.slug}`}>
                             {e.name}
                         </Link>
